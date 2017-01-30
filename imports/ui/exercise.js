ff4bf5baa5ac1419ca8720046c0312f18161d1ed
Alkons <nocturn.designers@gmail.com>
2017-01-30 13:58:30 +0300

    updating styles for materialize

--- imports/ui/exercise.js
+++ imports/ui/exercise.js
@@ -8,3 +8,21 @@ Template.body.helpers({
         return Exercises.find({})
     }
 });
+
+Template.body.events({
+    'submit .new-task' (event) {
+        console.log("Event");
+        console.log(event);
+        event.preventDefault();
+
+        const target = event.target;
+        const name = target.name.value;
+
+        Exercises.insert({
+            name,
+            createdAt: new Date(),
+        });
+
+        target.name.value = '';
+    },
+});
